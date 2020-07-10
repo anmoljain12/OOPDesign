4078601cec5c5ec313f9a68a5cc243d90dc523c1
Anmol jain <anmoljain1207@gmail.com>
2020-07-11 01:15:20 +0530

    Added functionality in Gear.java: 	Added feature to compare bicycles with different wheel-size by calculating gear_inches value.
    	Member variables :
    		Added:  (double)rim - rim diameter in inches
    			(double)tire - tire diameter in inches
    
    	Methods:
    		Added :  gear_inches() - calculates and return gear_inches value
    		Modified:   Gear(chainRing, cog, rim, tire) - modified class constructor to accept rim and tire parameters.

--- Gear.java
+++ Gear.java
@@ -10,19 +10,27 @@ public class Gear{
 
 	int chainRing;
 	int cog;
+	double rim;
+	double tire;
 
-	public Gear(int chainRing, int cog){
+	public Gear(int chainRing, int cog, double rim, double tire){
 		this.chainRing = chainRing;
 		this.cog = cog;
+		this.rim = rim;
+		this.tire = tire;
 	}
 
 	public double ratio(){
 		return chainRing/ (double) cog;
 	}
 
+	public double gearInches(){
+		return this.ratio()*(rim+(tire*2));
+	}
+
 
 	public static void main(String[] args) {
-		System.out.println((new Gear(52,11)).ratio());
-		System.out.println((new Gear(30,27)).ratio());
+		System.out.println((new Gear(52, 11, 26, 1.5)).gearInches());
+		System.out.println((new Gear(52, 11, 24, 1.25)).gearInches());
 	}
 }
