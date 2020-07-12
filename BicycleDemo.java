466d9e136be5c9de0eea3f13b761caf4978816ee
Anmol jain <anmoljain1207@gmail.com>
2020-07-13 01:10:23 +0530

    Added functionality in Bicycle: Can now represent mountain bike
    Mountain bikes and road bikes are much alike but there are clear differences between them. They have an overall bike size and a chain and tire size. The only differences between road and mountain bikes are that road bikes need handlebar tape and mountain bikes have suspension.The new front_shock and rear_shock variables hold mountain bike specific parts. The new style variable determines which parts appear on the spares list. The spares method now contains an if statement that checks the contents of the variable style to initialize spares in road and mountain bike resp.
    
    Bicycle class:
    	Member variables:
    		Added: style, rear_shock, front_hock
    	Methods:
    		Modified: Bicycle - class constructore now takes arguments as a map with key as variable name and value as the bariable value. This is done to make arguments independent of order and allow null values as well.
    			 getSpares - Added if else condition based on style value to get spares based on bike style.

--- BicycleDemo.java
+++ BicycleDemo.java
@@ -2,6 +2,7 @@
 javac -version
 javac BicycleDemo.java -d ClassFiles
 java -cp ClassFiles learnJava.BicycleDemo
+
 */
 package learnJava;
 
@@ -10,28 +11,45 @@ import java.util.Map;
 import java.util.Arrays;
 
 class Bicycle{
+	String style;
 	String size;
 	String tape_color;
+	String front_shock;
+	String rear_shock;
 	Map<String, String> spares;
 
-	public Bicycle(String size, String tape_color){
-		this.size = size;
-		this.tape_color = tape_color;
+	public Bicycle(Map<String, String> args){
+		style = args.get("style");
+		size = args.get("size");
+		tape_color = args.get("tape_color");
+		front_shock = args.get("front_shock");
+		rear_shock = args.get("rear_shock");
 		spares = getSpares();
 	}
 
 	private Map<String, String> getSpares(){
 		Map<String, String> newSpares = new HashMap<>();
-		newSpares.put("chain", "10-speed");
-		newSpares.put("tire_size", "23");
-		newSpares.put("tape_color", this.tape_color);
+		if (style.equals("road")){
+			newSpares.put("chain", "10-speed");
+			newSpares.put("tire_size", "23");
+			newSpares.put("tape_color", this.tape_color);
+		}else{
+			newSpares.put("chain", "10-speed");
+			newSpares.put("tire_size", "2.1");
+			newSpares.put("rear_shock", this.rear_shock);
+		}
 		return newSpares;
 	}
 }
 
 public class BicycleDemo{
-	public static void main(String[] args) {
-		Bicycle bike = new Bicycle("M", "red");
+	public static void main(String[] arg) {
+		Map<String, String> args = new HashMap<>();
+		args.put("style", "mountain");
+		args.put("size", "S");
+		args.put("front_shock", "Manitou");
+		args.put("rear_shock", "Fox");
+		Bicycle bike = new Bicycle(args);
 		System.out.println(bike.size);
 		System.out.println(Arrays.asList(bike.spares));
 	}
