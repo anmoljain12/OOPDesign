3bcec9c590915d2b2fd65fa61935caeb8303db75
Anmol jain <anmoljain1207@gmail.com>
2020-07-13 01:44:12 +0530

    Modified Bicycle class: Introduced inheritance by creating MountainBike as subclass of Bicycle
    The style variable effectively divides instances of Bicycle into two different kinds of things. These two things share a great deal of behavior but differ along the style dimension. This single class contains several different, but related, types. This is the exact problem that inheritance solves. Another class MountainBike is created which acts as subclass of Bicycle and has its own constructor and overrides getSpares. In MountainBike constructor the super class constructore is called first.
    
    Bicycle class:
    	Member variables:
    		Removed: style, rear_shock, front_shock
    	Methods:
    		Modified: Bicycle - initializes only size, tape_color and spares
    			  getSpares - removed style bases if else condition
    
    MountainBike class : extends Bicycle class
    	Member variables:
    		Added: rear_shock, front_shock
    	Methods:
    		Added: MountainBike - calls super class constructor and initialize rear_shocka nd front_shock
    		       getSpares - adds rear_shock spare in spares of super class
    
    Issues: The mountainbike still has member variables such as tire_size which are inherited from Bicycle class but not needed in MountainBike class. The Bicycle class contains behavior that is appropriate for both a peer and a parent of MountainBike. Some of the behavior in Bicycle is correct for MountainBike, some is wrong, and some doesn’t even apply. As written, Bicycle should not act as the superclass of MountainBike.

--- BicycleDemo.java
+++ BicycleDemo.java
@@ -11,35 +11,41 @@ import java.util.Map;
 import java.util.Arrays;
 
 class Bicycle{
-	String style;
 	String size;
 	String tape_color;
-	String front_shock;
-	String rear_shock;
 	Map<String, String> spares;
 
 	public Bicycle(Map<String, String> args){
-		style = args.get("style");
 		size = args.get("size");
 		tape_color = args.get("tape_color");
+		spares = getSpares();
+	}
+
+	private Map<String, String> getSpares(){
+		Map<String, String> newSpares = new HashMap<>();
+		newSpares.put("chain", "10-speed");
+		newSpares.put("tire_size", "23");
+		newSpares.put("tape_color", tape_color);
+		return newSpares;
+	}
+}
+
+class MountainBike extends Bicycle{
+	String front_shock;
+	String rear_shock;
+
+	public MountainBike(Map<String, String> args){
+		super(args);
 		front_shock = args.get("front_shock");
 		rear_shock = args.get("rear_shock");
 		spares = getSpares();
 	}
 
 	private Map<String, String> getSpares(){
-		Map<String, String> newSpares = new HashMap<>();
-		if (style.equals("road")){
-			newSpares.put("chain", "10-speed");
-			newSpares.put("tire_size", "23");
-			newSpares.put("tape_color", this.tape_color);
-		}else{
-			newSpares.put("chain", "10-speed");
-			newSpares.put("tire_size", "2.1");
-			newSpares.put("rear_shock", this.rear_shock);
-		}
-		return newSpares;
+		spares.put("rear_shock", rear_shock);
+		return spares;
 	}
+
 }
 
 public class BicycleDemo{
@@ -49,8 +55,8 @@ public class BicycleDemo{
 		args.put("size", "S");
 		args.put("front_shock", "Manitou");
 		args.put("rear_shock", "Fox");
-		Bicycle bike = new Bicycle(args);
-		System.out.println(bike.size);
-		System.out.println(Arrays.asList(bike.spares));
+		MountainBike mountain_bike = new MountainBike(args);
+		System.out.println(mountain_bike.size);
+		System.out.println(Arrays.asList(mountain_bike.spares));
 	}
 }
