1cac9a883a19a08709ee97c05bc6b849386caa86
Anmol jain <anmoljain1207@gmail.com>
2020-07-14 01:10:50 +0530

    Made RoadBike and MountainBike Subclasses of Bicycle
    The current Bicycle class intermingled general bicycle code with specific road bike code. Now, Bicycle is the superclass of both MountainBike and RoadBike. Bicycle will contain the common behavior, and MountainBike and RoadBike will add specializations. Bicycle’s public interface should include spares and size, and the interfaces of its subclasses will add their individual parts.
    
    Modified Bicycle - currently unimplementd, all code moved to Bicycle class
    Added RoadBike - contains code of Bicycle
    
    ToDo - Corrently roadbike will work fine, but mountainbike class will not work as it depends on its super class and its super class bicycle has currntly no implememtation. Bicycle class will be implemented in following commits.

--- BicycleDemo.java
+++ BicycleDemo.java
@@ -11,11 +11,15 @@ import java.util.Map;
 import java.util.Arrays;
 
 class Bicycle{
+	
+}
+
+class RoadBike extends Bicycle{
 	String size;
 	String tape_color;
 	Map<String, String> spares;
 
-	public Bicycle(Map<String, String> args){
+	public RoadBike(Map<String, String> args){
 		size = args.get("size");
 		tape_color = args.get("tape_color");
 		spares = getSpares();
@@ -30,6 +34,7 @@ class Bicycle{
 	}
 }
 
+/*
 class MountainBike extends Bicycle{
 	String front_shock;
 	String rear_shock;
@@ -47,16 +52,24 @@ class MountainBike extends Bicycle{
 	}
 
 }
-
+*/
 public class BicycleDemo{
 	public static void main(String[] arg) {
-		Map<String, String> args = new HashMap<>();
-		args.put("style", "mountain");
-		args.put("size", "S");
+		Map<String, String> args1 = new HashMap<>();
+		args1.put("size", "M");
+		args1.put("tape_color", "red");
+		RoadBike road_bike = new RoadBike(args1);
+		System.out.println(road_bike.size);
+		System.out.println(Arrays.asList(road_bike.spares));
+
+		/*
+		Map<String, String> args2 = new HashMap<>();
+		args2.put("size", "S");
 		args.put("front_shock", "Manitou");
-		args.put("rear_shock", "Fox");
-		MountainBike mountain_bike = new MountainBike(args);
+		args2.put("rear_shock", "Fox");
+		MountainBike mountain_bike = new MountainBike(args2);
 		System.out.println(mountain_bike.size);
 		System.out.println(Arrays.asList(mountain_bike.spares));
+		*/
 	}
 }
